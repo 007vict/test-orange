dbe8e19ee056b6effd2f1af853e353f10a2ba6be
007vict <super_vg@bk.ru>
2022-03-15 10:41:54 +0300

    feat: create Footer and css property

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import classes from '../css/Footer.module.css'
+import container from '../css/Container.module.css'
 
 export default function Footer() {
   return (
-    <footer className='Footer'>
-      <h3>Footer</h3>
+    <footer className={classes.footer}>
+      <div className={container.container}>
+        <div>
+          <h6>© 2014 - 2021 Tapston Development, LLC</h6>
+        </div>
+        <div className={classes.clutch}>
+          <div className={classes.logo_wrapper}>
+            <span className={classes.logo_text}>Reviewed on</span>
+            <a
+              className={classes.logotype}
+              target="_blank"
+              href="https://clutch.co/profile/tapston?utm_source=widget&utm_medium=2&utm_campaign=widget&utm_content=logo#summary"></a>
+          </div>
+          <div className={classes.cont_stars}>
+            <div className={classes.fivestars}>
+              <span class="active"></span>
+              <span class="active"></span>
+              <span class="active"></span>
+              <span></span>
+              <span></span>
+            </div>
+            <span class={classes.reviews_count}>
+              <a href="https://clutch.co/profile/tapston?utm_source=widget&amp;utm_medium=2&amp;utm_campaign=widget&amp;utm_content=num_reviews#reviews">
+                21 reviews
+              </a>
+            </span>
+          </div>
+        </div>
+      </div>
     </footer>
-  )
+  );
 }
