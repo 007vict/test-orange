631abea709ed6a6d003db90a41f0d002a30b4f57
007vict <super_vg@bk.ru>
2022-09-02 19:30:09 +0300

    fix: rewiev hash router

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import { Provider } from 'react-redux';
 import store from "./store/store";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom"
+import {BrowserRouter, HashRouter} from "react-router-dom"
 import './css/index.module.css';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root'),
 );
 reportWebVitals();
